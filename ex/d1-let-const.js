ddebad0ed176f44ff991cc91cd4c51acc5dec79b
Max <forlearncodemm2020.gmail.com>
2025-08-23 18:56:33 +0800

    start learn d1 and d2

---
+++ ex/d1-let-const.js
@@ -0,0 +1,12 @@
+let age = 35 ;
+const name = 'Max';
+
+console.log(age , name)
+
+let a = 10;
+let b = 5 ;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ ex/d2-if-else.js
@@ -0,0 +1,13 @@
+let score =60
+
+
+
+if(score >= 90){
+    console.log("優秀");
+}
+else if(score >= 60 && score <= 89){
+    console.log("合格");
+}
+else{
+    console.log("不合格");
+}
